8c70c6cc1d0121dd605743a03bc1f046dc267e5b
Vinay Kumar Thakur <vinaykumar.thakur@lpu.in>
2023-03-24 02:41:21 +0530

    js file

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+/*-------- toggle icon navbar --------*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
 /*--------scroll sections active link --------*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -16,4 +25,33 @@ window.onscroll = () => {
             });
         };
     });
+    /*-------- sticky navbar --------*/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*-------- remove toggle icon and navbar when click navbar link (scroll) --------*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
+/*-------- scroll reveal --------*/
+ScrollReveal({
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+/*-------- typed js --------*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Student', 'Junior Developer', 'Cybersecurity Enthusiast!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
