62a9624b69f7156e34f643dfdc34cffbfb5f5ad4
Vinay Kumar Thakur <vinaykumar.thakur@lpu.in>
2023-05-01 23:38:54 +0530

    js file

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
-
+// Vinay Kumar Thakur
 /*-------- typed js --------*/
 const typed = new Typed('.multiple-text', {
     strings: ['Student', 'Junior Web Developer', 'Cybersecurity Enthusiast!'],
